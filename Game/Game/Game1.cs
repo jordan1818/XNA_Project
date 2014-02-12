ebce93d3425ee8ab939e0c9a9e1dab39073a022a
Zachary Massia <zmassia@gmail.com>
2014-02-12 15:50:54 -0500

    Setup ScreenManager

--- Game/Game/Game1.cs
+++ Game/Game/Game1.cs
@@ -9,9 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using ECS;
-using Game.Systems;
-using Game.Components;
-using Game.Template;
+using Game.StateManagement.ScreenManager;
 
 namespace Game
 {
@@ -20,11 +18,7 @@ namespace Game
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
 
-        private Matrix viewMatrix, projectionMatrix;
-
-        private EntityWorld entityWorld;
-
-        private Entity player;
+        private ScreenManager screenManager;
 
         public Game1()
         {
@@ -34,34 +28,11 @@ namespace Game
 
         protected override void Initialize()
         {
-            InitMatrices();
             InitBlackBoard();
-            InitEntityWorld();
-
+            screenManager = new ScreenManager(this);
             base.Initialize();  // Must be called last.
         }
 
-        /// <summary>
-        /// Calculate the view and projection matrices.
-        /// </summary>
-        private void InitMatrices()
-        {
-            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, GraphicsDevice.Viewport.AspectRatio, 1.0f, 1000f);
-
-            viewMatrix = Matrix.CreateLookAt(Vector3.Zero, -Vector3.UnitZ, Vector3.Up);
-        }
-
-        /// <summary>
-        /// Create the entity world and register systems.
-        /// </summary>
-        private void InitEntityWorld()
-        {
-            entityWorld = new EntityWorld();
-
-            // Register the systems.
-            entityWorld.RegisterSystem<MovementSystem>();
-            entityWorld.RegisterSystem<RenderSystem>();
-        }
 
         /// <summary>
         /// Register objects needed across multiple systems.
@@ -71,62 +42,30 @@ namespace Game
             BlackBoard.SetEntry("ContentManager"  , Content);
             BlackBoard.SetEntry("GraphicsDevice"  , GraphicsDevice);
             BlackBoard.SetEntry("SpriteBatch"     , spriteBatch);
-            BlackBoard.SetEntry("ProjectionMatrix", projectionMatrix);
-            BlackBoard.SetEntry("ViewMatrix"      , viewMatrix);
         }
 
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            player = entityWorld.CreateFromTemplate<PlayerTemplate>();
+            screenManager.LoadContent();
         }
 
         protected override void UnloadContent()
         {
+            screenManager.UnloadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
-                GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.Back))
-            {
-                Exit();
-            }
-
-            // Debug controls. Input will be moved into the ECS in the future.
-            var transform = player.GetComponent<TransformComponent>();
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
-            {
-                transform.Position += new Vector3(0, 0, -1);
-            } 
-            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
-            {
-                transform.Position += new Vector3(0, 0, 1);
-            }
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                var rot = Quaternion.CreateFromYawPitchRoll(0, 0, 0.25f);
-                transform.Rotation *= rot;
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                var rot = Quaternion.CreateFromYawPitchRoll(0, 0, -0.25f);
-                transform.Rotation *= rot;
-            }
-
-            entityWorld.Update(gameTime.ElapsedGameTime);
+            screenManager.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            graphics.GraphicsDevice.Clear(Color.Black);
 
-            spriteBatch.Begin();
-            entityWorld.Draw();
-            spriteBatch.End();
+            screenManager.Draw(gameTime);
         }
     }
 }
