a6aa31953711a9739d484d71ade0de6b7976b583
jordan1818 <jordan18188@hotmail.com>
2014-02-12 15:05:24 -0500

    Initial Commit

---
+++ Game/Game/Template/PlayerTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ECS;
+
+namespace Game.Template
+{
+    class PlayerTemplate : IEntityTemplate
+    {
+        public PlayerTemplate()
+        {
+        }
+
+        public Entity Build(Entity e)
+        {
+            return e;
+        }
+   
+    }
+}
