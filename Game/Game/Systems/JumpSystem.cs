04b04cbf3f63d89a41324d523b3b228a099b64d2
jordan1818 <jordan18188@hotmail.com>
2014-03-18 12:45:18 -0400

    background implemented

---
+++ Game/Game/Systems/JumpSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using ECS;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Game.Components;
+
+namespace Game.Systems
+{
+    class JumpSystem : EntitySystem
+    {
+        public JumpSystem(EntityWorld entityWorld) :
+            base(entityWorld, new Type[] { typeof(VelocityComponent), typeof(TransformComponent), typeof(JumpComponent) }, GameLoopType.Update)
+        {
+        }
+
+        protected override void Process(Entity entity)
+        {
+            var vel = entity.GetComponent<VelocityComponent>();
+            var transform = entity.GetComponent<TransformComponent>();
+            var jump = entity.GetComponent<JumpComponent>();
+
+            if (jump.JumpCount > jump.MAXJUMPS)
+                jump.WantToJump = false;
+
+            else if (jump.WantToJump && jump.JumpCount < jump.MAXJUMPS)
+                jump.JumpCount += 1;
+                jump.WantToJump = false;
+
+            if (transform.Position.Y <= 0.0f)
+                jump.JumpCount = 0;
+
+
+        }
+    }
+}
